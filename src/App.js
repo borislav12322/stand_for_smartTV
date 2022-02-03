df57f658e262f6d1fa1c16340364f63557f31eaa
Borislav <borislavizmestiev@gmail.com>
2022-02-03 19:11:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Title</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur debitis, deleniti dignissimos ducimus et exercitationem illo in magni modi molestiae neque placeat quaerat quibusdam, rem sint soluta tenetur voluptatibus? Aliquam aspernatur at, beatae cupiditate dolorem facilis illo illum ipsum, itaque iusto laudantium minima nulla quaerat, quam recusandae sint sit suscipit ut veritatis vitae. Molestias nesciunt obcaecati provident saepe vel.Ad dolorem eum id ipsum iste labore molestias provident quaerat? Deserunt dolor error est molestiae quia quo similique! Accusantium amet aperiam aut autem earum laborum officiis perferendis provident reprehenderit sit?
+            </p>
+            <iframe src="https://borislav12322.github.io/smart-tv-ad/"></iframe>
+        </div>
+    );
 }
 
 export default App;
